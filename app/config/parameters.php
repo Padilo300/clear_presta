d899197781be01bb40e0a469417a31e2ee170f9e
padilo300 <padilo300@gmail.com>
2019-06-16 19:11:58 +0300

    first

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'padilo00.mysql.tools',
+    'database_port' => '',
+    'database_name' => 'padilo00_devpre',
+    'database_user' => 'padilo00_devpre',
+    'database_password' => '1ng(1LGt)8',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'ieuqMFfpewXz6kNv2Js34yenab115HJsbDGry0P6VbZiYM8to6kuHC0q',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2019-06-16',
+    'locale' => 'ru-RU',
+    'cookie_key' => 'OfCHeHPEXepQLqSoDxDDotIYX3NemoubTpoNZV2aZbbCJyZ05CC4akRn',
+    'cookie_iv' => 'yNkSiJFu',
+    'new_cookie_key' => 'def00000092584200e1b0bea61809399f0a3e4cca3e46c5b383bef3f5efdd3d5a52c1786527fcb2b228c4b404ea29d823d72a65c5d8565ec3df91b22bc5c3db4cf63b1c6',
+  ),
+);
